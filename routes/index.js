6ed068e667c8670fa82ae41816a58bc04c157daa
MohGanji <mohammad-ganji@outlook.com>
2017-07-31 17:56:31 +0430

    first test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.sendStatus(200);
+});
+
+router.post('/', function(req, res){
+    console.log(req.body);
 });
 
 module.exports = router;
