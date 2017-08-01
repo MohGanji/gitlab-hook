175d1056d6114d15285dc4d8dc9fd1f976283544
MohGanji <mohammad-ganji@outlook.com>
2017-08-01 14:17:53 +0430

    adding configs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var configs = require('./configs.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,10 +8,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-    console.log("---headers: ", req.headers);
-    console.log("---specific header: ", req.header("X-Gitlab-Token"));
-    console.log("---query: ", req.query);
-    console.log("---body: ", req.body);
+    var serverToken = req.header("X-Gitlab-Token");
+    var gitEvent = req.header("x-gitlab-event");
+    var userEmail = req.body.user_email;
+  
+    console.log(configs.secretToken);
+    console.log(serverToken, gitEvent, userEmail);
+    // if(serverToken != configs.secretToken)
+  
+    // console.log("---headers: ", req.headers);
+    // console.log("---specific header: ", req.header("X-Gitlab-Token"));
+    // console.log("---query: ", req.query);
+    // console.log("---body: ", req.body);
     res.send(200, "Ok");
 });
 
